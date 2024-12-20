cd7f3386911ca0bf6abc2b0e184e7fb9b2ebadbc
June Wu <junewu1@fareastone.com.tw>
2024-12-20 17:36:57 +0800

    feat: 初版

---
+++ script.js
@@ -0,0 +1,55 @@
+const { createApp, ref } = Vue
+
+const app = createApp({
+	setup() {
+		const menuActive = ref(false)
+		const menuItems = ref([
+			{
+				label: "歡迎蒞臨",
+				url: "#home",
+			},
+			{
+				label: "訂房方案",
+				url: "#plan",
+			},
+			{
+				label: "關於流連",
+				url: "#about",
+			},
+			{
+				label: "交通資訊",
+				url: "#traffic",
+			},
+			{
+				label: "周邊機能",
+				url: "#function",
+			},
+			{
+				label: "景點指南",
+				url: "#tourist",
+			},
+		])
+
+		const clickMenu = () => {
+			menuActive.value = !menuActive.value
+		}
+
+		return {
+			menuActive,
+			menuItems,
+			clickMenu,
+		}
+	},
+})
+
+app.use(PrimeVue.Config, {
+	theme: {
+		preset: PrimeVue.Themes.Aura,
+	},
+})
+
+app.component("p-drawer", PrimeVue.Drawer)
+app.component("p-menu", PrimeVue.Menu)
+app.component("p-button", PrimeVue.Button)
+
+app.mount("#app")
